bed0742bdad2559a37a046ab889bddbe64b4a3c9
Vadim Shepelev <vadim.aries@gmail.com>
2017-05-08 20:43:13 +0300

    ready to heroku

--- sb/www/views.py
+++ sb/www/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    posts = BlogPost.objects.all()
+    return render(request, 'index.html', {'posts':posts})
